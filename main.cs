c41c83b936e5daf4176925e59c2f7689e1812a8c
Cj Johnston <64555196+therainsmelody@users.noreply.github.com>
2021-10-24 02:07:30 -0400

    Add files via upload

---
+++ main.cs
@@ -0,0 +1,34 @@
+using System;
+
+class MainClass {
+  
+
+  
+  public static void Main (string[] args)
+  
+   {
+   
+   Console.WriteLine("Enter the first side of the triangle:");
+   double side1 = Convert.ToDouble(Console.ReadLine());
+
+   Console.WriteLine("Enter the first side of the triangle:");
+   double side2 = Convert.ToDouble(Console.ReadLine());
+
+  
+   
+
+{
+  DetermineHypotenuse();
+}
+  
+  public static void DetermineHypotenuse(ref double side1, ref double side2)
+   {
+     double hypotenuse=0;
+ hypotenuse = Math.Sqrt((side1 * side1) + (side2 * side2) );
+
+  Console.WriteLine("The Hypotenuse is = " + hypotenuse);
+   
+  }
+   
+  }
+}
